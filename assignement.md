e2a84f5ea228ca2a8345d1268715913de65fd2b7
Amine El Ghayate <amine@elgh.fr>
2015-12-26 12:11:53 +0100

    assignement done

---
+++ assignement.md
@@ -0,0 +1,104 @@
+##Hervé P.
+-DOM
+-JSON
+-Node.js
+
+##Semere B.
+-WebGL
+-jQuer
+y-JSCS
+
+##Marie S.
+-JSHint
+-Promise
+-Ember
+
+##Thomas B.
+-Bluebird
+-XHR
+-Yeoman
+
+##Mehdi H.
+-webpack
+-Isomorphic
+-AJAX
+
+##Samar A.
+-Nightmare
+-Metalsmith
+-Moment.js
+
+##Andréa B.
+-Browserify
+-IIFE
+-Flux
+
+##Alice J.
+-JSLint
+-RxJS
+-React
+
+##Robin F.
+-Babel
+-Chai
+-Sails
+
+##Allan C.
+-Hoisting
+-Mocha
+-Three.js
+
+##Thomas G.
+-Gulp
+-ECMAScript
+-V8
+
+##Karine L.
+-JSON-LD
+-Backbone
+-Universal
+
+##Sébastien J.
+-Jasmine
+-CouchDB
+-Grunt
+
+##Nicolas S.
+-Broccoli
+-JSX
+
+##Benjamin D.
+-CORS
+-MEAN
+
+##Anis L.
+-Canvas
+-CoffeeScript
+
+##Jérôme M.
+-Express
+-AngularJS
+
+##Justin C.
+-D3.js
+-TypeScript
+
+##Hayate S.
+-Knockout
+-ESLint
+
+##Louis M.
+-Redux
+-Underscore
+
+##Floriane G.
+-Brunch
+-Hapi
+
+##Oriane M.
+-Bower
+-npm
+
+##Yoann S.
+-nvm
+-AMD
--- repart.php
+++ repart.php
@@ -8,11 +8,12 @@ class AssignPeople {
 	public function __construct(){
 		$this->loadFiles();
 		$this->randomize();
+		$this->assign();
 		$this->output();
 	}
 
 	public function loadFiles(){
-		$sjsj = file_get_contents('README.md');
+		$tasks = file_get_contents('README.md');
 		$this->tasks = explode("\n", $tasks);
 		
 		$ppl = file_get_contents("https://smp.ovh/ppl.json");
@@ -23,13 +24,53 @@ class AssignPeople {
 	}
 
 	public function randomize(){
-		shuffle($this->sjsj);
+		shuffle($this->tasks);
 		shuffle($this->ppl);
 	}
 
+	public function assign(){
+		$i = 0;
+		foreach($this->tasks as $task){
+			$this->ppl[$i]->terms[] = $task;
+			$i++;
+			if(!isset($this->ppl[$i])){
+				$i = 0;
+			} 
+		}
+	}
+
 	public function output(){
-		var_dump($this->sjsj);
-		var_dump(json_encode($this->ppl));
+		//var_dump($this->tasks);
+		//var_dump(json_encode($this->ppl));
+		$this->confirm();
+		$this->toMd();
+	}
+
+	public function toMd(){
+		echo 'outputing file';
+		$out = [];
+		foreach($this->ppl as $p) {
+			$out[] = '##' . $p->name;
+			foreach($p->terms as $term) {
+				$out[] = '-' . $term;
+			}
+			$out[] = '';
+		}
+
+		echo join("\n", $out);
+	}
+
+	public function confirm(){
+		echo "Ecrire le fichier md?  (y/N): ";
+		$handle = fopen ("php://stdin","r");
+		$line = fgets($handle);
+		fclose($handle);
+		if(trim($line) != 'y'){
+		    echo "ABORTING!\n";
+		    exit;
+		}
+		echo "\n"; 
+		echo "Thank you, continuing...\n";
 	}
 
 }
@@ -37,6 +78,7 @@ class AssignPeople {
 class Human {
 
 	public $name;
+	public $terms = [];
 
 	public function __construct($humanObject){
 		$this->name = ucfirst($humanObject->first_name) . ' ' . substr(ucfirst($humanObject->last_name),0,1) .'.';
